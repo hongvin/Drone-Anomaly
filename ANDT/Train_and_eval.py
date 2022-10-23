82610460f8d80c90f8235554ae67ae9500a3266c
Koay Hong Vin <32212455+hongvin@users.noreply.github.com>
2022-10-23 16:24:38 +0800

    Update Train_and_eval.py

--- ANDT/Train_and_eval.py
+++ ANDT/Train_and_eval.py
@@ -302,7 +302,7 @@ def test(model, test_batch, test_label, num_frames, device):
 
 
 def main():
-    path = '../drone-anomaly/Drone-Anomaly/'
+    path = '../input/drone-anomaly/Drone-Anomaly/'
     data_provider(path)
 
 
